e5566a6de06b158f87d777d110505aaa3071ce4f
Atrax7 <frimin.zen@yandex.ru>
2023-04-21 16:41:17 +0300

    It is amayzing

--- proverka.py
+++ proverka.py
@@ -1,5 +1,12 @@
+from math import sqrt
+
+def hipotinus(a, b):
+    return sqrt(a ** 2 + b ** 2)
+
+
 print("come to the dark side.")
 
 print("I see you")
 
 print("I always come back")
+print(hipotinus(4, 3))
